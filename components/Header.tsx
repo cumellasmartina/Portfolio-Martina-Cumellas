f43eae13b7baf4f8228fdf9d57ee851aa8063c9f
Martina Cumellas <cumellasmartina@gmail.com>
2022-12-17 09:14:14 +0100

    feat: implement hero component

---
+++ components/Header.tsx
@@ -0,0 +1,67 @@
+import { motion } from "framer-motion";
+import React from "react";
+import { SocialIcon } from "react-social-icons";
+
+type Props = {};
+
+export default function Header({}: Props) {
+  return (
+    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center ">
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center"
+      >
+        {/* Social Icons */}
+        <SocialIcon
+          url="https://www.linkedin.com/in/martina-cumellas/"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://github.com/cumellasmartina"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+      </motion.div>
+
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center text-gray-300 cursor-pointer"
+      >
+        <SocialIcon
+          className="cursor-pointer"
+          network="email"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+          Reach me!
+        </p>
+      </motion.div>
+    </header>
+  );
+}
